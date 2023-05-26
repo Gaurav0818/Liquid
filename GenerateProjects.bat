77ce9ecc2b620f69887a1858f5fbd8989de8103a
Gaurav0818 <Gaurav.ps.0818@gmail.com>
2023-05-26 08:16:34 +0530

    Changed Project Setup from Premake To Cmake

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,22 @@
-call vender\premake\premake5.exe vs2022
-PAUSE
+@echo off
+
+rem Specify the build directory
+set BUILD_DIR=build
+
+rem Create the build directory if it doesn't exist
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+
+rem Change directory to the build directory
+cd %BUILD_DIR%
+
+rem Configure the CMake project
+cmake ..
+
+rem Build the project using the generated build system
+cmake --build .
+
+rem Return to the original directory
+cd ..
+
+rem Pause to keep the command prompt open after completion
+pause
